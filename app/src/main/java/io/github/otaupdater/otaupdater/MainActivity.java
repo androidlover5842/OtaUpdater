88525ddb88484d0a2058c52bd3f80045173a1285
grace5921 <androidlover5842@gmail.com>
2016-12-21 12:22:40 +0530

    add on click listner for check for update button

--- app/src/main/java/io/github/otaupdater/otaupdater/MainActivity.java
+++ app/src/main/java/io/github/otaupdater/otaupdater/MainActivity.java
@@ -1,21 +1,32 @@
 package io.github.otaupdater.otaupdater;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import io.github.otaupdater.otaupdater.fragment.GithubReleasesFragment;
 
 public class MainActivity extends AppCompatActivity {
-
     private GithubReleasesFragment mFragmentOldRelease;
+    private Button mCheckUpdate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.mFragmentOldRelease = new GithubReleasesFragment().setTargetURL(Config.URL_OLD_RELEASES);
         updateFragment(mFragmentOldRelease);
+        mCheckUpdate=(Button)findViewById(R.id.activity_main_check_for_update);
+        mCheckUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getBaseContext(), DialogActivity.class);
+                startActivity(i);
+            }
+        });
     }
     protected void updateFragment(Fragment fragment)
     {

7758cf80ee24184d305cb2de99dfe4e1df73ff12
grace5921 <androidlover5842@gmail.com>
2017-01-15 13:18:51 +0530

    flasher: show dialog before flashing

--- app/src/main/java/io/github/otaupdater/otaupdater/activity/OpenScriptGenerator.java
+++ app/src/main/java/io/github/otaupdater/otaupdater/activity/OpenScriptGenerator.java
@@ -3,12 +3,15 @@ package io.github.otaupdater.otaupdater.activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SwitchCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import com.eminayar.panter.PanterDialog;
+import com.eminayar.panter.enums.Animation;
 
 import eu.chainfire.libsuperuser.Shell;
 import io.github.otaupdater.otaupdater.R;
@@ -25,6 +28,7 @@ public class OpenScriptGenerator extends AppCompatActivity {
     private Button FlashButton;
     private String p,SCRIPT_PATH = "/cache/recovery/openrecoveryscript";
     private boolean WipeData,WipeCache;
+    private PanterDialog FlashDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +40,33 @@ public class OpenScriptGenerator extends AppCompatActivity {
         Path=(TextView)findViewById(R.id.file_path_textView);
         FlashButton=(Button)findViewById(R.id.flash_button);
         p=getPreferences(OpenScriptGenerator.this,"FilePath");
+        FlashDialog= new PanterDialog(OpenScriptGenerator.this);
+
+        mWipeCache.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mWipeCache.isChecked()) {
+                    Snackbar.make(v, "Cache wipe enabled", Snackbar.LENGTH_LONG).show();
+                }
+                else {
+                    Snackbar.make(v, "Cache wipe disabled", Snackbar.LENGTH_LONG).show();
+
+                }
+
+            }
+        });
+        mWipeData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mWipeData.isChecked()) {
+                    Snackbar.make(v, "Data wipe enabled", Snackbar.LENGTH_SHORT).show();
+                }
+                else {
+                    Snackbar.make(v, "Data wipe disabled", Snackbar.LENGTH_SHORT).show();
+
+                }
+            }
+        });
 
         Path.setText(p);
         FlashButton.setOnClickListener(new View.OnClickListener() {
@@ -43,15 +74,11 @@ public class OpenScriptGenerator extends AppCompatActivity {
             public void onClick(View v) {
                 if(mWipeData.isEnabled())
                 {
-                    Toast.makeText(OpenScriptGenerator.this, "Data will wiped",
-                            Toast.LENGTH_LONG).show();
                     WipeData=true;
 
                 }
                 if(mWipeCache.isEnabled())
                 {
-                    Toast.makeText(OpenScriptGenerator.this, "Cache will wiped",
-                            Toast.LENGTH_SHORT).show();
                     WipeCache=true;
 
                 }
@@ -59,13 +86,31 @@ public class OpenScriptGenerator extends AppCompatActivity {
                 Shell.SU.run("touch "+SCRIPT_PATH);
                 Shell.SU.run("echo ' install "+p+" ' > "+ SCRIPT_PATH);
                 if(WipeData==true){
-                    Shell.SU.run("echo 'install wipe data' >> "+ SCRIPT_PATH);
+                    Shell.SU.run("echo ' install wipe data ' >> "+ SCRIPT_PATH);
                 }
                 if(WipeCache==true){
-                    Shell.SU.run("echo 'install wipe cache' >> "+ SCRIPT_PATH);
+                    Shell.SU.run("echo ' install wipe cache ' >> "+ SCRIPT_PATH);
                 }
-                Shell.SU.run("reboot recovery");
 
+                FlashDialog.setTitle("Are you sure ?")
+                        .setHeaderBackground(R.color.colorPrimaryDark)
+                        .setMessage(p)
+                        .setPositive("Flash", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Shell.SU.run("reboot recovery");
+
+                            }
+                        })
+                        .setNegative("Cancel", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                FlashDialog.dismiss();
+                            }
+                        })
+                        .isCancelable(false)
+                        .withAnimation(Animation.SIDE);
+                FlashDialog.show();
             }
         });
 

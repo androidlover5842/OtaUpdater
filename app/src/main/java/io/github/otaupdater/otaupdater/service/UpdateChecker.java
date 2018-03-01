63b6b9d4e2eaa3f43202cf3ef926b5d7e6628141
androidlover5842 <androidlover5842@gmail.com>
2018-03-01 23:46:18 +0530

    update gradle and cleanup

--- app/src/main/java/io/github/otaupdater/otaupdater/service/UpdateChecker.java
+++ app/src/main/java/io/github/otaupdater/otaupdater/service/UpdateChecker.java
@@ -7,7 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
-import android.support.v7.app.NotificationCompat;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 

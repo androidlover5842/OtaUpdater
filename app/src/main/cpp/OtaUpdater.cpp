601b739aefc137d64cb767069d464aaeeef93e43
androidlover5842 <androidlover5842@gmail.com>
2018-03-02 12:07:46 +0530

    run command from native lib

---
+++ app/src/main/cpp/OtaUpdater.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string>
+#include <unistd.h>
+#include <dirent.h>
+#include <jni.h>
+#include <bits/stdc++.h>
+#include <android/log.h>
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+#define  LOG_TAG    "FROM LIB"
+
+string exec(const char* cmd) {
+    char buffer[128];
+    string result = "";
+    FILE* pipe = popen(cmd, "r");
+    if (!pipe) throw std::runtime_error("popen() failed!");
+    try {
+        while (!feof(pipe)) {
+            if (fgets(buffer, 128, pipe) != NULL)
+                result += buffer;
+        }
+    } catch (...) {
+        pclose(pipe);
+        throw;
+    }
+    pclose(pipe);
+    return result;
+}
+
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_io_github_otaupdater_otaupdater_activity_MainActivity_Runcommand(JNIEnv *env,jobject jobj,
+                                                                      jstring command) {
+    const char *convert = env->GetStringUTFChars(command, NULL);
+    string co=exec(convert);
+    return env->NewStringUTF(co.c_str());
+}
